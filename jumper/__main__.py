9aa18646d098b19038fe5b20a4198d1dbf90fe61
Michael Verhulst <michael@terminallabs.com>
2020-11-21 22:06:52 -0600

    code cleaning

--- jumper/__main__.py
+++ jumper/__main__.py
@@ -1,3 +1,32 @@
-from jumper.cli import main
+import os
+import sys
+import click
+import pytest
 
-main()
+@click.group()
+def cli():
+    return None
+
+
+@click.group(name="system")
+def system_group():
+    return None
+
+
+@system_group.command(name="version")
+def version_command():
+    print("not implemented")
+
+
+@system_group.command(name="selftest")
+def selftest_command():
+    print("not implemented")
+
+
+@system_group.command(name="selfcoverage")
+def selfcoverage_command():
+    print("not implemented")
+
+
+cli.add_command(system_group)
+main = cli
--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
+import os
+import tempfile
+
+APPDIR = os.path.abspath(os.path.dirname(__file__))
+SETUPFILEDIR = os.path.abspath(os.path.join(APPDIR, ".."))
+TESTDIR = os.path.abspath(os.path.join(APPDIR, "tests"))
+MEMTEMPDIR = "/dev/shm"
+
+if os.path.isdir(MEMTEMPDIR):
+    tempfile.tempdir = MEMTEMPDIR
+
 import sys
 from setuptools import setup, find_packages
 
-from jumper.settings import *
+VERSION = "2020.1.1"
+PRINT_VERBOSITY = "high"
+EXCLUDED_DIRS = [".DS_Store"]
+PROJECT_NAME = "jumper"
+TEMPDIR = "/tmp"
+TEXTTABLE_STYLE = ["-", "|", "+", "-"]
+DIRS = [f"{TEMPDIR}/jumperworkingdirs"]
+MINIMUM_PYTHON_VERSION = (3, 6, 0)
+COVERAGERC_PATH = f"{APPDIR}/.coveragerc"
 
 assert sys.version_info >= MINIMUM_PYTHON_VERSION
 
@@ -19,6 +38,6 @@ setup(
     ],
     entry_points="""
         [console_scripts]
-        jumper=jumper.cli:main
+        jumper=jumper.__main__:main
      """,
 )
